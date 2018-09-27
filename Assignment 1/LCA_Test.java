123cff6461ff9ebb49a47fe7030afbc4453bbea4
cassidke <cassidke@tcd.ie>
2018-09-27 13:51:48 +0100

    Creating J-unit test file

--- Assignment 1/LCA_Test.java
+++ Assignment 1/LCA_Test.java
@@ -1,1 +1,18 @@
+import org.Junit.test;
 
+public class LCA_Test 
+{
+ 	@Test
+	public void testIsEmpty() 
+ 	{
+		LCA test = new LCA();
+		boolean isEmpty = test.isEmpty();
+		assertEquals(isEmpty, true);
+		test.put(1, 2);
+		test.put(2, 4);
+		test.put(3, 6);
+		test.put(4, 8);
+		isEmpty = test.isEmpty();
+		assertEquals(isEmpty, false);
+	}
+ }
