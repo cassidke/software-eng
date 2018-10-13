29f69782b74bfe8b1efb5e84cb72d048dc03aa71
cassidke <43173821+cassidke@users.noreply.github.com>
2018-10-13 14:16:25 +0100

    Outline test methods in DAG_Test.java

--- Assignment 1/DAG_Test.java
+++ Assignment 1/DAG_Test.java
@@ -7,4 +7,33 @@ import org.junit.runners.JUnit4;
 
 public class DAG_Test{
 	
+	@Test
+	public void testDAG()
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void addEdge()
+	{
+		
+	}
+	
+	@Test
+	public void testIndegree()
+	{
+		
+	}
+	
+	@Test 
+	public void testV()
+	{
+		
+	}
+	
+	@Test
+	public void testAdj()
+	{
+		
+	}
 }
