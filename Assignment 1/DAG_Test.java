43dd23f1eaa492044135467bded8a7f56193dd74
cassidke <43173821+cassidke@users.noreply.github.com>
2018-10-17 14:38:16 +0100

    Additional LCA tests

--- Assignment 1/DAG_Test.java
+++ Assignment 1/DAG_Test.java
@@ -140,6 +140,56 @@ public class DAG_Test {
 		assertEquals(6, graph.findLCA(6, 7));
 	}
 	
+	@Test
+	public void testLCAforNoCommonAncestors()
+	{
+		DAG graph = new DAG(10);
+		graph.addEdge(0, 1);
+		graph.addEdge(0, 3);
+		graph.addEdge(1, 3);
+		graph.addEdge(3, 2);
+		graph.addEdge(2, 4);
+		graph.addEdge(1, 5);
+		graph.addEdge(2, 5);
+		
+		//Check LCA function works
+		assertEquals(0, graph.findLCA(2, 1));
+		assertEquals(3, graph.findLCA(2, 3));
+		assertEquals(2, graph.findLCA(4, 5));
+		//
+		assertEquals(-1, graph.findLCA(8, 2));
+	}
 	
+	@Test
+	public void testLCAForNonDAG()
+	{
+		DAG graph = new DAG(10);
+		
+		graph.addEdge(0, 1);
+		graph.addEdge(0, 3);
+		graph.addEdge(3, 2);
+		graph.addEdge(2, 0);
+		graph.addEdge(2, 4);
+		
+		assertEquals(-1, graph.findLCA(3, 2));
+		assertEquals(-1, graph.findLCA(2, 4));
+		assertEquals(-1, graph.findLCA(1, 3));
+		assertEquals(-1, graph.findLCA(0, 3));
+		assertEquals(-1, graph.findLCA(1, 2));
+		
+	}
+	
+	@Test
+	public void testLCAForSameVertex()
+	{
+		DAG graph = new DAG(10);
+		
+		graph.addEdge(0, 1);
+		graph.addEdge(0, 3);
+		graph.addEdge(1, 3);
+		
+		assertEquals(3, graph.findLCA(3, 3));
+		
+	}
 	
 }
